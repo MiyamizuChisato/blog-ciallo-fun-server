73889d6c1a3077b9c8a311aeabe110b85d590f70
chisato <MiyamizuChisato@gmail.com>
2023-08-23 09:12:58 +0800

    code

--- src/main/java/fun/ciallo/blog/controller/UserProfileController.java
+++ src/main/java/fun/ciallo/blog/controller/UserProfileController.java
@@ -1,6 +1,7 @@
 package fun.ciallo.blog.controller;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
 import cn.xuyanwu.spring.file.storage.FileInfo;
 import cn.xuyanwu.spring.file.storage.FileStorageService;
 import fun.ciallo.blog.dto.UserProfileUpdateDto;
@@ -9,12 +10,15 @@ import fun.ciallo.blog.security.Open;
 import fun.ciallo.blog.service.UserIdentityService;
 import fun.ciallo.blog.service.UserProfileService;
 import fun.ciallo.blog.vo.UserProfileVo;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.time.Duration;
 import java.util.List;
 
 @RestController
@@ -26,14 +30,26 @@ public class UserProfileController {
     private UserIdentityService userIdentityService;
     @Resource
     private FileStorageService fileStorageService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Open(HttpMethod.GET)
     @GetMapping("/{id}")
+
     public UserProfileVo get(@PathVariable int id) {
+        String userProfileJson = stringRedisTemplate.opsForValue().get("user:profile:" + id);
+        if (StringUtils.hasLength(userProfileJson)) {
+            return JSONUtil.toBean(userProfileJson, UserProfileVo.class);
+        }
         UserProfile userProfile = userProfileService.getById(id);
         List<Integer> identityIds = userIdentityService.getIdentityIdsByUser(userProfile.getId());
         UserProfileVo userProfileVo = BeanUtil.copyProperties(userProfile, UserProfileVo.class);
         userProfileVo.setIdentityIds(identityIds);
+        stringRedisTemplate.opsForValue().set(
+                "user:profile:" + id,
+                JSONUtil.toJsonStr(userProfileVo),
+                Duration.ofMinutes(20L)
+        );
         return userProfileVo;
     }
 
@@ -54,5 +70,6 @@ public class UserProfileController {
             userProfile.setAvatar(avatarInfo.getUrl());
         }
         userProfileService.updateById(userProfile);
+        stringRedisTemplate.delete("user:profile:" + userProfile.getId());
     }
 }
